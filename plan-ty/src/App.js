b8304e386b5764d1c9fe49b2913c9571406152ca
Brigita <brigita.ce@gmail.com>
2024-05-13 21:47:08 +0200

    feature navBar, home,modified App.js

--- plan-ty/src/App.js
+++ plan-ty/src/App.js
@@ -1,13 +1,24 @@
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/navBar/NavBar";
+import WaterTemp from "./components/parameters/waterTemp/WaterTemp";
 
-import './App.css';
-import IotComponent from './iotDataReceiver';
-import WaterTemp from './components/parameters/waterTemp/WaterTemp'
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <div className="App">
-      
-      <WaterTemp/>
+      <Router>
+        <div>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/waterTemp" element={<WaterTemp />} />
+            <Route path="*" element={<div>404 Not Found</div>}></Route>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
