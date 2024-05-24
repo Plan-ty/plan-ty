2ee79a5f26028fa4fd1a9aca26f378248bf8f548
Brigita <brigita.ce@gmail.com>
2024-05-24 12:27:36 +0200

    feature MotorToggle created

--- plan-ty/src/components/home/Home.jsx
+++ plan-ty/src/components/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import MotorToggle from "../motorToggle/MotorToggle";
 
 const Home = () => {
   return (
     <div id="home">
       <h1>Welcome to the Home Page</h1>
+      <MotorToggle />
       <p>This is a test page to check if navigation works.</p>
     </div>
   );
