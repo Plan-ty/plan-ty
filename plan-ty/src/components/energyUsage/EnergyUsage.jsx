5aa9a8972dcfa8911e018f17a64591d460b3106a
Catarina Jesus <catarinaqj@gmail.com>
2024-05-27 14:02:49 +0200

    Requirement 27 done

---
+++ plan-ty/src/components/energyUsage/EnergyUsage.jsx
@@ -0,0 +1,36 @@
+    import React, { useState, useEffect } from "react";
+    import axios from "axios";
+    import '../energyUsage/EnergyUsage.css';
+
+
+
+    const EnergyUsage = () => {
+
+        const [energy, setEnergyUsage] = useState('');
+       
+        useEffect(() => {
+            energyUsage();
+          }, []);
+        
+          const energyUsage = async () => {
+            try {
+              const response = await axios.get("http://localhost:5021");
+              setEnergyUsage(response);
+            } catch (error) {
+              console.error("Error getting Energy Usage:", error);
+            }
+          };
+    
+
+
+          return (
+            <div className="energyUsage">
+                <p>Energy Usage(J): {energy} </p>
+
+            </div>
+          )
+    };
+
+
+
+    export default EnergyUsage;
